1095f70132461ae985f4a86af8256de5d2125187
cosmin <50593959+cosmin-andrei@users.noreply.github.com>
2024-11-13 22:41:50 +0200

    login function Participant class and repo

--- Organisation/src/main/java/org/example/Model/Organisation.java
+++ Organisation/src/main/java/org/example/Model/Organisation.java
@@ -19,6 +19,8 @@ public class Organisation extends User implements Serializable {
     @Column(name = "name", nullable = false, length = 255)
     private String name;
 
+
+
     @Column(name = "creation_date")
     @Convert(converter = LocalDateConverter.class)
     private LocalDate creationDate;
