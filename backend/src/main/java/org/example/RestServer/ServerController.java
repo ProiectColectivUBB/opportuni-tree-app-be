3c248dda4ef5a6910d33ecadfb5f59e6af5897c2
Vlad123abc <vladi.ivanescu@gmail.com>
2024-11-14 17:11:45 +0200

    implemented register api

--- backend/src/main/java/org/example/RestServer/ServerController.java
+++ backend/src/main/java/org/example/RestServer/ServerController.java
@@ -2,6 +2,8 @@ package org.example.RestServer;
 
 import org.example.Model.OpportunityReqDTO;
 import org.example.Model.OpportunityRespDTO;
+import org.example.Model.Organisation;
+import org.example.Model.Participant;
 import org.example.Service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -42,12 +44,46 @@ public class ServerController
         }
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/register")
-    public ResponseEntity<?> register()
+    @RequestMapping(method = RequestMethod.GET, value = "/register/organisation")
+    public ResponseEntity<?> register_organisation(@RequestParam("username") String username,
+                                                   @RequestParam("password") String password,
+                                                   @RequestParam("about") String about,
+                                                   @RequestParam("phone") String phone,
+                                                   @RequestParam("name") String name,
+                                                   @RequestParam("creationDateStr") String creationDateStr,
+                                                   @RequestParam("website") String website)
     {
-        System.out.println("register called");
+        System.out.println("register organisation called");
         try {
-            return new ResponseEntity<>(HttpStatus.OK);
+            Optional<Organisation> saved_org = service.registerOrganisation(username, password, about, phone, name, creationDateStr, website);
+            if (saved_org.isPresent())
+            {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/register/organisation")
+    public ResponseEntity<?> register_participant(@RequestParam("username") String username,
+                                                   @RequestParam("password") String password,
+                                                   @RequestParam("about") String about,
+                                                   @RequestParam("phone") String phone,
+                                                   @RequestParam("first_name") String first_name,
+                                                   @RequestParam("last_name") String last_name,
+                                                   @RequestParam("birthDateStr") String birthDateStr)
+    {
+        System.out.println("register participant called");
+        try {
+            Optional<Participant> saved_part = service.registerParticipant(username, password, about, phone, first_name, last_name, birthDateStr);
+            if (saved_part.isPresent())
+            {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
         catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
