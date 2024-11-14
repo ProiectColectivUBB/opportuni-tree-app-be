3f66a77c7a3cef8afe5ce0750d9fcf75b50412a7
Vlad123abc <vladi.ivanescu@gmail.com>
2024-11-14 16:12:55 +0200

    api work + major code fix

--- backend/src/main/java/org/example/Service/Service.java
+++ backend/src/main/java/org/example/Service/Service.java
@@ -1,34 +1,49 @@
 package org.example.Service;
 
-import org.example.Model.Organisation;
-import org.example.Model.Participant;
+import org.example.Model.*;
+import org.example.Persistence.OpportunityPhotosRepository;
+import org.example.Persistence.OpportunityRepository;
 import org.example.Persistence.OrganisationRepository;
 import org.example.Persistence.ParticipantRepository;
 import org.example.Utils.PasswordHasher;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+@org.springframework.stereotype.Service
 public class Service {
 
-    private final OrganisationRepository organisationRepo;
-    private final ParticipantRepository participantRepo;
+    private SessionFactory sessionFactory = new Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(Opportunity.class)
+            .addAnnotatedClass(OpportunityPhoto.class)
+            .addAnnotatedClass(User.class)
+            .addAnnotatedClass(Participant.class)
+            .addAnnotatedClass(Organisation.class)
+            .buildSessionFactory();
+    private final OrganisationRepository organisationRepo = new OrganisationRepository(sessionFactory);
+    private final ParticipantRepository participantRepo = new ParticipantRepository(sessionFactory);
 
-    public Service(OrganisationRepository organisationRepo, ParticipantRepository participantRepo) {
-        this.organisationRepo = organisationRepo;
-        this.participantRepo = participantRepo;
-    }
+    private final OpportunityRepository opportunityRepository=new OpportunityRepository(sessionFactory);
+    private final OpportunityPhotosRepository opportunityPhotosRepository=new OpportunityPhotosRepository(sessionFactory);
 
-    public List<Organisation> findAll() {
+    public List<Organisation> findAllOrganisations() {
         return organisationRepo.findAll();
     }
 
-    public Optional<Organisation> findOne(Long id) {
+    public Optional<Organisation> findOrganisation(Long id) {
         return organisationRepo.findOne(id);
     }
 
-    public Organisation save(Organisation organisation) {
+    public Organisation saveOrganisation(Organisation organisation) {
 
         organisation.setPassword(PasswordHasher.hashPassword(organisation.getPassword()));
         return organisationRepo.save(organisation);
@@ -49,11 +64,11 @@ public class Service {
         return Optional.empty();
     }
 
-    public Organisation update(Organisation organisation) {
+    public Organisation updateOrganisation(Organisation organisation) {
         return organisationRepo.update(organisation);
     }
 
-    public Organisation delete(Long id) {
+    public Organisation deleteOrganisation(Long id) {
         return organisationRepo.delete(id);
     }
 
@@ -134,6 +149,67 @@ public class Service {
 
     public Optional<Participant> deleteParticipant(Long id) {
         return Optional.ofNullable(participantRepo.delete(id));
+    }
+
+    public void addOpportunity(OpportunityReqDTO opportunityDTO){
+        List<OpportunityPhoto> opportunityPhotos=new ArrayList<>();
+        Opportunity opportunity = new Opportunity(opportunityDTO.getName(),opportunityDTO.getType(),opportunityDTO.getCity(),
+                opportunityDTO.getCountry(),opportunityDTO.getDescription(),opportunityDTO.getPrice(),
+                opportunityDTO.getNoParticipants(),opportunityDTO.getOrganisationId(),opportunityPhotos);
+        Integer id = opportunityRepository.addOpportunity(opportunity);
+
+        opportunityDTO.getPhotos().forEach(multipartFile -> {
+            try {
+                String photoURL=saveFile(multipartFile);
+                OpportunityPhoto opportunityPhoto=new OpportunityPhoto(id,photoURL);
+                opportunityPhotos.add(opportunityPhoto);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        opportunityPhotosRepository.addOpportunityPhotos(opportunityPhotos);
+    }
+
+    private String saveFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        String folderPath = "../uploads";
+        Path filePath = Paths.get(folderPath, fileName);
+        Files.copy(file.getInputStream(), filePath);
+        return filePath.toString();
+    }
+
+    public List<OpportunityRespDTO> getAllOpportunities() {
+        List<OpportunityRespDTO> opportunityDTOS = new ArrayList<>();
+        List<Opportunity> opportunities = opportunityRepository.getAllOpportunities();
+
+        opportunities.forEach(opportunity -> {
+            List<String> fileBase64s = new ArrayList<>();
+
+            opportunity.getPhotos().forEach(opportunityPhoto -> {
+                String photoBase64 = null;
+                try {
+                    // Convertește fișierul în Base64
+                    photoBase64 = convertFileToBase64(opportunityPhoto.getPhotoUrl());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                fileBase64s.add(photoBase64);
+            });
+
+            OpportunityRespDTO opportunityDTO = new OpportunityRespDTO(
+                    opportunity.getName(), opportunity.getType(), opportunity.getCity(),
+                    opportunity.getCountry(), opportunity.getDescription(), opportunity.getPrice(),
+                    opportunity.getNoParticipants(), opportunity.getOrganisationId(), fileBase64s
+            );
+            opportunityDTOS.add(opportunityDTO);
+        });
+        return opportunityDTOS;
+    }
+
+    private String convertFileToBase64(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        byte[] photoBytes = Files.readAllBytes(path);
+        return Base64.getEncoder().encodeToString(photoBytes);
     }
 
 }
